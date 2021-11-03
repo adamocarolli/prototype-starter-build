68bfe37b83395c0b58308693495422300d24b69a
adamocarolli <adamo.carolli@gmail.com>
2021-11-03 18:54:59 +0000

    Deploying to gh-pages from  @ 2e5bdf77404ce407e9965e6b73f2f799f00cf1fe 🚀

---
+++ about.js
@@ -0,0 +1,5 @@
+(()=>{var e=`<img class="app-logo" />
+<h1 class="app-name">Contact App</h1>
+<img class="add-icon" />
+`;var p="./logo-47ADSLHM.png";var i="./add-2EFLUXPD.png";function m(){let t=document.createElement("div");t.className="header",t.innerHTML=e,t.querySelector(".app-logo").setAttribute("src",p),t.querySelector(".add-icon").setAttribute("src",i),document.body.appendChild(t)}m();})();
+//# sourceMappingURL=about.js.map
